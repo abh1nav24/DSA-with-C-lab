f309e376243990519d0d031372b900f633291d98
abh1nav24 <abhinavamoli24@gmail.com>
2026-08-21 13:42:03 +0530

    Add string reversal using stack data structure
    Implement a stack to reverse strings without recursion.

---
+++ Week_4_Q1(without recursion).c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#define MAX 1000
+
+char stack[MAX];
+int top = -1;
+
+void push(char ch)
+{
+    stack[++top] = ch;
+}
+
+char pop()
+{
+    return stack[top--];
+}
+
+int main()
+{
+    int T;
+    char str[MAX];
+
+    scanf("%d", &T);
+
+    while (T--)
+    {
+        scanf("%s", str);
+
+        top = -1;
+
+        int i = 0;
+
+        // Push all characters
+        while (str[i] != '\0')
+        {
+            push(str[i]);
+            i++;
+        }
+
+        // Pop and print in reverse
+        while (top != -1)
+        {
+            printf("%c", pop());
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
